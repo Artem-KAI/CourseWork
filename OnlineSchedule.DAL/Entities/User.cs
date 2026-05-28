e7784196423e7f0f70a1f850279ff1fb1ec9e827
ArtemKAI <9255411@stud.kai.edu.ua>
2026-05-29 01:46:55 +0300

    base entity

---
+++ OnlineSchedule.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
+    // Admin, Editor, Management, Teacher, Student
+    public string Role { get; set; }
+}
