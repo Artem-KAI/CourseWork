260c8732966acb278d1426222345558f524dbc34
ArtemKAI <9255411@stud.kai.edu.ua>
2026-06-04 14:56:41 +0300

    fixed previous commit

--- OnlineSchedule.DAL/Interfaces/IRepository.cs
+++ OnlineSchedule.DAL/Interfaces/IRepository.cs
@@ -2,5 +2,9 @@
 
 public interface IRepository<T> where T : class
 {
-
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetAsync(int id);
+    Task CreateAsync(T item);
+    void Update(T item);
+    Task DeleteAsync(int id);
 }
