c6832b2e1595a1066112d00bb79531912023ca9a
Даня Євтушенко <evtusenkodana@gmail.com>
2026-06-04 22:05:10 +0300

    Added WebAPI request models

---
+++ OnlineSchedule.WebAPI/Models/ScheduleRequests.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI.Models
+{
+    public class CreateScheduleItemRequest
+    {
+        public int GroupId { get; set; }
+        public int TeacherId { get; set; }
+        public int ClassroomId { get; set; }
+        public int DisciplineId { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public int LessonNumber { get; set; }
+        public string WeekType { get; set; } = string.Empty;
+    }
+
+    public class UpdateScheduleItemRequest
+    {
+        public int GroupId { get; set; }
+        public int TeacherId { get; set; }
+        public int ClassroomId { get; set; }
+        public int DisciplineId { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public int LessonNumber { get; set; }
+        public string WeekType { get; set; } = string.Empty;
+    }
+}
