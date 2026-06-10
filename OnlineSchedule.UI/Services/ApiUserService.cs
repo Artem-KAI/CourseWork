de38ae720ec11d5d23cc142c6495c09f0e2cb0a5
Даня Євтушенко <evtusenkodana@gmail.com>
2026-06-10 05:57:24 +0300

    Implemented /users with all features. Fixed bugs

--- OnlineSchedule.UI/Services/ApiUserService.cs
+++ OnlineSchedule.UI/Services/ApiUserService.cs
@@ -45,7 +45,7 @@ namespace UI.Services
             var client = CreateClient();
 
             return await client.GetFromJsonAsync<List<UserInfo>>
-                ("api/user") ?? new();
+                ("api/users") ?? new();
         }
 
         public async Task<UserInfo?> GetUserAsync(int id)
@@ -53,23 +53,23 @@ namespace UI.Services
             var client = CreateClient();
 
             return await client.GetFromJsonAsync<UserInfo>
-                ($"api/user/{id}");
+                ($"api/users/{id}");
         }
 
-        public async Task UpdateUserAsync(int id, UserInfo user)
+        public async Task UpdateUserAsync(int id, UpdateUserRequests request)
         {
             var client = CreateClient();
 
             await client.PutAsJsonAsync(
                 $"api/users/{id}",
-                user);
+                request);
         }
 
         public async Task DeleteUserAsync(int id)
         {
             var client = CreateClient();
 
-            await client.DeleteAsync($"api/user/{id}");
+            await client.DeleteAsync($"api/users/{id}");
         }
     }
 }
